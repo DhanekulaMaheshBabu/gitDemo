7928f8df65bde1f0f93865b832bbc711ca2b0a32
Dhanekula Mahesh Babu <dhanekula.maheshbabu5@gmail.com>
2025-02-12 12:36:06 +0530

    Intial Commit

---
+++ calculator.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number sir/madam: "))
+b = int(input("Enter second number sir/madam: "))
+
+print("Sum:", a + b)
+print("Difference:", a - b)
+print("Multiplication", a*b)
